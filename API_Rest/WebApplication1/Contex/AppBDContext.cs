002ffa8d23e4e482ecd57ae3e0f05a83b03d0222
AlejandroQL23 <ale23200@gmail.com>
2024-06-27 15:30:59 -0600

    c# API
    Basic example of API

---
+++ API_Rest/WebApplication1/Contex/AppBDContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebAPIPerson.Contex
+{
+    public class AppBDContext : DbContext
+    {
+        public AppBDContext(DbContextOptions<AppBDContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
